c3114fb466dd97596f392acbefc82d9551eb3c55
matic.podpadec <matic.podpadec@gmail.com>
2017-08-13 19:58:00 +0200

    fixing bugs

--- Light/lights.py
+++ Light/lights.py
@@ -4,6 +4,6 @@ import RPi.GPIO as GPIO
 def single_light_control(pin, intensity):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin, GPIO.OUT)
-
+    intensity = intensity / 100
     light = GPIO.PWM(pin, intensity)
     light.start(0)
