404f327ac61e6b6f9e941a99ef3b82473b3a2d9b
matic.podpadec <matic.podpadec@gmail.com>
2017-08-14 15:12:15 +0200

    pass to Pi

--- Light/lights.py
+++ Light/lights.py
@@ -4,21 +4,12 @@ import time
 
 
 def single_light_control(a, b):
-    GPIO.cleanup()
     GPIO.setmode(GPIO.BCM)
+
     pin = int(a)
     intensity = float(b)
+
     GPIO.setup(pin, GPIO.OUT)
     light = GPIO.PWM(pin, 100)
 
-    if GPIO.gpio_function(pin) != GPIO.OUT:
-        light.start(intensity)
-
-    else:
-        if intensity > 0:
-            light.ChangeDutyCycle(intensity)
-
-        else:
-            GPIO.cleanup()
-
-    time.sleep(5)
+    light.start(intensity)
